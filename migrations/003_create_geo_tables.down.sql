331c06b78460e25c2493441b737b268e5be36311
Dwiputra Sam Mulia <84729084+samdyra@users.noreply.github.com>
2024-08-16 22:56:33 +0700

    refactor: migration files

--- migrations/003_create_geo_tables.down.sql
+++
@@ -1,5 +0,0 @@
--- Drop the index
-DROP INDEX IF EXISTS idx_geo_data_list_table_name;
-
--- Drop the geo_data_list table
-DROP TABLE IF EXISTS geo_data_list;
---
+++ migrations/03_create_layer_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS layer (
+    id SERIAL PRIMARY KEY,
+    data_id INTEGER REFERENCES data(id),
+    layer_name VARCHAR(100) NOT NULL,
+    coordinate POINT,
+    color VARCHAR(7),
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    created_by INTEGER REFERENCES users(id),
+    updated_by INTEGER REFERENCES users(id)
+);
